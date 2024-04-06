12efe0ae057e8b2c9b1cd1d572d3d918f0662834
Cong Nghia IT <nghia2801dt@gmail.com>
2024-04-06 16:49:06 +0700

    Nghia Commit

--- NhapASM_ShopThuoc/src/main/java/vn/webbanthuoc/controller/web/SanPhamController.java
+++ NhapASM_ShopThuoc/src/main/java/vn/webbanthuoc/controller/web/SanPhamController.java
@@ -10,24 +10,78 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import vn.webbanthuoc.dao.clazzDao;
+import vn.webbanthuoc.dao.ThuocDao;
 import vn.webbanthuoc.entity.Thuoc;
 
-
-
-
-@WebServlet("/sanPhamController")
+@WebServlet({ "/sanPhamController", "/sanPhamChiTietController" })
 public class SanPhamController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	ThuocDao thuocDao = new ThuocDao();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 
-		clazzDao thuocDao = new clazzDao();
-		List<Thuoc> listThuoc = thuocDao.findAll();
-		request.setAttribute("ListUser", listThuoc);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		switch (action) {
+		case "/sanPhamController":
+			SanPham(request, response);
+			break;
+
+		case "/sanPhamChiTietController":
+			SanPhamChitiet(request, response);
+			break;
+
+		}
+	}
+
+	private void SanPhamChitiet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String method = request.getMethod();
+		if (method.equalsIgnoreCase("GET")) {
+			String id = request.getParameter("id");
+			Thuoc thuoc = thuocDao.findById(id);
+			request.setAttribute("thuoc", thuoc);
+			request.getRequestDispatcher("/views/web/productDetail.jsp").forward(request, response);
+		}
+	}
+
+	private void SanPham(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("aplication/json");
+		String fullname = request.getParameter("fullname");
+		int page = 0; // Trang mặc định là 0 (trang đầu tiên)
+		int pageSize = 4; // Số lượng mục trên mỗi trang
+
+		if (request.getParameter("page") != null) {
+			page = Integer.parseInt(request.getParameter("page"));
+		}
+
+		ThuocDao thuocDao = new ThuocDao();
+		List<Thuoc> listUsers;
+
+		if (fullname != null && !fullname.isEmpty()) {
+			listUsers = thuocDao.findByFullname(fullname);
+		} else if (request.getParameter("chkHangMoiConnect") != null) {
+			listUsers = thuocDao.findThuocNew();
+		}else if(request.getParameter("OptionHightoLow")!=null) {
+			listUsers=thuocDao.findPriceHigh();
+		} 
+		else {
+			listUsers = thuocDao.findAll(page, pageSize);
+		}
+		long sum = thuocDao.countAll(); // Tính tang số lượng user
+		long totalPages = (long) Math.ceil((double) sum / pageSize); // Tính tổng số trang
+
+		request.setAttribute("ListUser", listUsers);
+		request.setAttribute("TotalPages", totalPages);
+		request.setAttribute("CurrentPage", page);
 		RequestDispatcher rd = request.getRequestDispatcher("/views/web/sanpham.jsp");
 		rd.forward(request, response);
 	}
-	
+
 }
--- NhapASM_ShopThuoc/src/main/java/vn/webbanthuoc/dao/ThuocDao.java
+++ NhapASM_ShopThuoc/src/main/java/vn/webbanthuoc/dao/ThuocDao.java
@@ -1,19 +1,79 @@
 package vn.webbanthuoc.dao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
 import vn.webbanthuoc.entity.Thuoc;
+import vn.webbanthuoc.util.JpaUtil;
 
-public interface ThuocDao {
-	Thuoc findById(String id);
+public class ThuocDao {
 
-	List<Thuoc> findAll();
+	public List<Thuoc> findByFullname(String fullname) {
+		EntityManager em = JpaUtil.getEntityManager();
+		String jpaQuery = "SELECT u FROM Thuoc u WHERE u.ten LIKE :ten";
+		TypedQuery<Thuoc> query = em.createQuery(jpaQuery, Thuoc.class);
+		query.setParameter("ten", "%" + fullname + "%");
+		return query.getResultList();
+	}
 
-	List<Thuoc> findAll(int pageNumber, int pageSize);
+	public List<Thuoc> findAll() {
+		EntityManager em = JpaUtil.getEntityManager();
+		String jpql = "SELECT s FROM Thuoc s order by s.idThuoc";
+		TypedQuery<Thuoc> query = em.createQuery(jpql, Thuoc.class);
+		return query.getResultList();
+	}
 
-	Thuoc create(Thuoc entity);
+	public List<Thuoc> findAll(int page, int pageSize) {
+		EntityManager em = JpaUtil.getEntityManager();
+		String jpaQuery = "SELECT s FROM Thuoc s order by s.idThuoc";
+		TypedQuery<Thuoc> query = em.createQuery(jpaQuery, Thuoc.class);
+		query.setFirstResult(page * pageSize);
+		query.setMaxResults(pageSize);
+		return query.getResultList();
+	}
 
-	Thuoc update(Thuoc entity);
+	public List<Thuoc> findThuocNew() {
+		EntityManager em = JpaUtil.getEntityManager();
+		// Sử dụng SimpleDateFormat để chuyển đổi chuỗi thành đối tượng Date
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date ngaySanXuat = null;
+		try {
+			ngaySanXuat = dateFormat.parse("2024-04-03");
+		} catch (ParseException e) {
+			// Xử lý ngoại lệ nếu có lỗi xảy ra trong quá trình chuyển đổi
+			e.printStackTrace();
+		}
 
-	Thuoc delete(Thuoc entity);
+		String jpql = "SELECT s FROM Thuoc s WHERE s.ngaySanXuat >= :ngay";
+		TypedQuery<Thuoc> query = em.createQuery(jpql, Thuoc.class);
+		query.setParameter("ngay", ngaySanXuat);
+
+		return query.getResultList();
+	}
+
+	public List<Thuoc> findPriceHigh() {
+	    EntityManager em = JpaUtil.getEntityManager();
+	    String jpql = "SELECT s FROM Thuoc s ORDER BY s.gia DESC";
+	    TypedQuery<Thuoc> query = em.createQuery(jpql, Thuoc.class);
+	    return query.getResultList();
+	}
+
+	public long countAll() {
+		EntityManager em = JpaUtil.getEntityManager();
+		TypedQuery<Long> query = em.createQuery("SELECT COUNT(u) FROM Thuoc u", Long.class);
+		return query.getSingleResult();
+	}
+
+	public Thuoc findById(String id) {
+		EntityManager em = JpaUtil.getEntityManager();
+		Thuoc entity = em.find(Thuoc.class, id);
+		return entity;
+	}
+
 }
