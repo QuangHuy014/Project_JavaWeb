d6e075fc129fdf6b91f58557673479cb017ec22f
Cong Nghia IT <nghia2801dt@gmail.com>
2024-04-10 22:44:03 +0700

    no message

--- NhapASM_ShopThuoc/src/main/java/vn/webbanthuoc/controller/web/CartProduct.java
+++ NhapASM_ShopThuoc/src/main/java/vn/webbanthuoc/controller/web/CartProduct.java
@@ -86,3 +86,4 @@ public class CartProduct extends HttpServlet {
 	        }
 	   }
 }
+
--- NhapASM_ShopThuoc/src/main/java/vn/webbanthuoc/controller/web/CheckoutSevlet.java
+++ NhapASM_ShopThuoc/src/main/java/vn/webbanthuoc/controller/web/CheckoutSevlet.java
@@ -19,85 +19,79 @@ import vn.webbanthuoc.entity.hoaDonChiTiet;
 /**
  * Servlet implementation class CheckoutSevlet
  */
-@WebServlet({"","/checkout"})
+@WebServlet({ "", "/checkout" })
 public class CheckoutSevlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    HoaDonDao hoaDonDao=new HoaDonDao();
-    HoaDonChiTietDao HoaDonChiTietDao=new HoaDonChiTietDao();
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CheckoutSevlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	HoaDonDao hoaDonDao = new HoaDonDao();
+	HoaDonChiTietDao HoaDonChiTietDao = new HoaDonChiTietDao();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/views/web/order_confirm.jsp").forward(request, response);
-		
+	public CheckoutSevlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/views/web/order_confirm.jsp").forward(request, response);
 
+	}
 
-	    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	            throws ServletException, IOException {
-	        // Lấy thông tin từ form
-	        String tenKhachHang = request.getParameter("tenKhachHang");
-	        String diaChi = request.getParameter("diaChi");
-	        String email = request.getParameter("email");
-	        String soDienThoai = request.getParameter("soDienThoai");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Lấy thông tin từ form
+		String tenKhachHang = request.getParameter("tenKhachHang");
+		String diaChi = request.getParameter("diaChi");
+		String email = request.getParameter("email");
+		String soDienThoai = request.getParameter("soDienThoai");
 
-	        // Tạo đối tượng hoaDon và lưu vào cơ sở dữ liệu
-	        try {
-	        	  hoaDon hoaDon = new hoaDon();
-	  	        hoaDon.setIDMaNV(9);
-	  	        hoaDon.setIDKhachHang(9);
-	  	        hoaDon.setIDHoaDon(1);
-	  	        hoaDon.setTenKhachHang(tenKhachHang);
-	  	        hoaDon.setDiaChiKhachHang(diaChi);
-	  	        hoaDon.setEmail(email);
-	  	        hoaDon.setSoDienThoaiKhachHang(soDienThoai);
-	  	        hoaDon.setNgayDH(new Date());
-	  	        hoaDonDao.create(hoaDon);
-			} catch (Exception e) {
-				System.out.println("loi insert hoa don ");
-				e.printStackTrace();
+		// Tạo đối tượng hoaDon và lưu vào cơ sở dữ liệu
+		try {
+			hoaDon hoaDon = new hoaDon();
+			hoaDon.setIDMaNV(9);
+			hoaDon.setIDKhachHang(9);
+			hoaDon.setIDHoaDon(1);
+			hoaDon.setTenKhachHang(tenKhachHang);
+			hoaDon.setDiaChiKhachHang(diaChi);
+			hoaDon.setEmail(email);
+			hoaDon.setSoDienThoaiKhachHang(soDienThoai);
+			hoaDon.setNgayDH(new Date());
+			hoaDonDao.create(hoaDon);
+		} catch (Exception e) {
+			System.out.println("loi insert hoa don ");
+			e.printStackTrace();
+		}
+
+		// Lấy giỏ hàng từ session
+		Map<String, Thuoc> cartThuoc = (Map<String, Thuoc>) request.getSession().getAttribute("cartThuocss");
+		try {
+			// Lưu từng sản phẩm trong giỏ hàng vào cơ sở dữ liệu
+			for (Thuoc thuoc : cartThuoc.values()) {
+				hoaDonChiTiet hoaDonChiTiet = new hoaDonChiTiet();
+				hoaDonChiTiet.setTenThuoc(thuoc.getTen());
+				hoaDonChiTiet.setIDThuoc(thuoc.getIdThuoc());
+				hoaDonChiTiet.setSoLuong(thuoc.getQuantity());
+				hoaDonChiTiet.setGia(thuoc.getGia());
+				hoaDonChiTiet.setDonVi(thuoc.getDonVi());
+				HoaDonChiTietDao.save(hoaDonChiTiet);
 			}
-	      
-	        
 
-	        // Lấy giỏ hàng từ session
-	        Map<String, Thuoc> cartThuoc = (Map<String, Thuoc>) request.getSession().getAttribute("cartThuocss");
-try {
-	   // Lưu từng sản phẩm trong giỏ hàng vào cơ sở dữ liệu
-    for (Thuoc thuoc : cartThuoc.values()) {
-        hoaDonChiTiet hoaDonChiTiet = new hoaDonChiTiet();
-        hoaDonChiTiet.setTenThuoc(thuoc.getTen());
-        hoaDonChiTiet.setIDThuoc(thuoc.getIdThuoc());
-        hoaDonChiTiet.setSoLuong(thuoc.getQuantity());
-        hoaDonChiTiet.setGia(thuoc.getGia());
-        hoaDonChiTiet.setDonVi(thuoc.getDonVi());
-        HoaDonChiTietDao.save(hoaDonChiTiet);
-    }
-	
-} catch (Exception e) {
-	System.out.println("loi them hoa don chi tiet");
-	e.printStackTrace();
-}
-	     
+		} catch (Exception e) {
+			System.out.println("loi them hoa don chi tiet");
+			e.printStackTrace();
+		}
 
-	        // Sau khi lưu thành công, xóa giỏ hàng khỏi session
-	        request.getSession().removeAttribute("cartThuocss");
+		// Sau khi lưu thành công, xóa giỏ hàng khỏi session
+		request.getSession().removeAttribute("cartThuocss");
 
-	        // Chuyển hướng đến trang thanh toán thành công hoặc trang khác
-	        response.sendRedirect("checkout-success.jsp");
-	    }
-	    
-	
-	    
-
+		// Chuyển hướng đến trang thanh toán thành công hoặc trang khác
+		response.sendRedirect("checkout-success.jsp");
+	}
 
 }
